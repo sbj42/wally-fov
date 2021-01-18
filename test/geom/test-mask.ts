6957f0ad092167e2c088cc7c43009dede2a521f3
James Clark <jameswclark@gmail.com>
2021-01-18 13:29:03 -0500

    assert.equal is deprecated

--- test/geom/test-mask.ts
+++ test/geom/test-mask.ts
@@ -6,15 +6,15 @@ describe('geom/mask', () => {
     describe('#constructor()', () => {
         it('starts filled with false', () => {
             const o = new geom.Mask({width: 10, height: 11});
-            assert.equal(o.width, 10);
-            assert.equal(o.height, 11);
-            assert.equal(o.get({x: 1, y: 2}), false);
+            assert.strictEqual(o.width, 10);
+            assert.strictEqual(o.height, 11);
+            assert.strictEqual(o.get({x: 1, y: 2}), false);
         });
         it('can be filled with true', () => {
             const o = new geom.Mask({width: 10, height: 11}, true);
-            assert.equal(o.width, 10);
-            assert.equal(o.height, 11);
-            assert.equal(o.get({x: 1, y: 2}), true);
+            assert.strictEqual(o.width, 10);
+            assert.strictEqual(o.height, 11);
+            assert.strictEqual(o.get({x: 1, y: 2}), true);
         });
     });
     describe('#toString()', () => {
@@ -23,16 +23,16 @@ describe('geom/mask', () => {
             o.set({x: 0, y: 0}, true);
             o.set({x: 1, y: 1}, true);
             o.set({x: 2, y: 1}, true);
-            assert.equal(o.toString(), '☑☐☐\n☐☑☑\n☐☐☐\n');
+            assert.strictEqual(o.toString(), '☑☐☐\n☐☑☑\n☐☐☐\n');
         });
     });
     describe('#set()', () => {
         it('works', () => {
             const o = new geom.Mask({width: 10, height: 11});
             o.set({x: 1, y: 2}, true);
-            assert.equal(o.get({x: 1, y: 2}), true);
+            assert.strictEqual(o.get({x: 1, y: 2}), true);
             o.set({x: 1, y: 2}, false);
-            assert.equal(o.get({x: 1, y: 2}), false);
+            assert.strictEqual(o.get({x: 1, y: 2}), false);
         });
     });
     describe('#forEach()', () => {
@@ -43,9 +43,9 @@ describe('geom/mask', () => {
             o.set({x: 2, y: 1}, true);
             o.forEach(new geom.Offset(), (cursor, value) => {
                 if (cursor.x === 2 && cursor.y === 1) {
-                    assert.equal(value, true);
+                    assert.strictEqual(value, true);
                 } else if (cursor.x === 1 && cursor.y === 2) {
-                    assert.equal(value, false);
+                    assert.strictEqual(value, false);
                 }
             });
         });
--- test/geom/test-offset.ts
+++ test/geom/test-offset.ts
@@ -5,58 +5,58 @@ import * as geom from '../../src/geom';
 describe('geom/offset', () => {
     it('starts at 0,0', () => {
         const o = new geom.Offset();
-        assert.equal(o.x, 0);
-        assert.equal(o.y, 0);
+        assert.strictEqual(o.x, 0);
+        assert.strictEqual(o.y, 0);
     });
     it('is mutable', () => {
         const o = new geom.Offset();
         o.x = 1;
-        assert.equal(o.x, 1);
+        assert.strictEqual(o.x, 1);
     });
     describe('#constructor()', () => {
         it('works', () => {
             const o = new geom.Offset(1, 2);
-            assert.equal(o.x, 1);
-            assert.equal(o.y, 2);
+            assert.strictEqual(o.x, 1);
+            assert.strictEqual(o.y, 2);
         });
     });
     describe('#set()', () => {
         it('works', () => {
             const o = new geom.Offset().set(1, 2);
-            assert.equal(o.x, 1);
-            assert.equal(o.y, 2);
+            assert.strictEqual(o.x, 1);
+            assert.strictEqual(o.y, 2);
         });
     });
     describe('#toString()', () => {
         it('works', () => {
-            assert.equal(new geom.Offset(1, 2).toString(), '(1,2)');
-            assert.equal(new geom.Offset(-3, -4).toString(), '(-3,-4)');
+            assert.strictEqual(new geom.Offset(1, 2).toString(), '(1,2)');
+            assert.strictEqual(new geom.Offset(-3, -4).toString(), '(-3,-4)');
         });
     });
     describe('#copyFrom()', () => {
         it('works', () => {
-            assert.equal(new geom.Offset().copyFrom(new geom.Offset(1, 2)).toString(), '(1,2)');
-            assert.equal(new geom.Offset().copyFrom({y: 2, x: 1}).toString(), '(1,2)');
+            assert.strictEqual(new geom.Offset().copyFrom(new geom.Offset(1, 2)).toString(), '(1,2)');
+            assert.strictEqual(new geom.Offset().copyFrom({y: 2, x: 1}).toString(), '(1,2)');
         });
     });
     describe('#addOffset()', () => {
         it('works', () => {
-            assert.equal(new geom.Offset(1, 2).addOffset({x: -3, y: -5}).toString(), '(-2,-3)');
+            assert.strictEqual(new geom.Offset(1, 2).addOffset({x: -3, y: -5}).toString(), '(-2,-3)');
         });
     });
     describe('#addCardinalDirection()', () => {
         it('works', () => {
-            assert.equal(new geom.Offset(1, 2).addCardinalDirection(geom.Direction.NORTH).toString(),
+            assert.strictEqual(new geom.Offset(1, 2).addCardinalDirection(geom.Direction.NORTH).toString(),
                 '(1,1)');
-            assert.equal(new geom.Offset(1, 2).addCardinalDirection(geom.Direction.EAST).toString(),
+            assert.strictEqual(new geom.Offset(1, 2).addCardinalDirection(geom.Direction.EAST).toString(),
                 '(2,2)');
-            assert.equal(new geom.Offset(1, 2).addCardinalDirection(geom.Direction.SOUTH).toString(),
+            assert.strictEqual(new geom.Offset(1, 2).addCardinalDirection(geom.Direction.SOUTH).toString(),
                 '(1,3)');
         });
     });
     describe('#subtractOffset()', () => {
         it('works', () => {
-            assert.equal(new geom.Offset(1, 2).subtractOffset({x: 3, y: 5}).toString(), '(-2,-3)');
+            assert.strictEqual(new geom.Offset(1, 2).subtractOffset({x: 3, y: 5}).toString(), '(-2,-3)');
         });
     });
 });
